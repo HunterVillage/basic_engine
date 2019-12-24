732f28cd70bfde0fb6e4883c55cd67416d2eb478
leyan95 <18231926271@163.com>
2019-12-24 11:41:02 +0800

    update input widget.

--- lib/widgets/form/input.dart
+++ lib/widgets/form/input.dart
@@ -9,6 +9,8 @@ class Input extends StatelessWidget {
   final String hint;
   final Widget trailing;
   final bool autofocus;
+  final bool hidden;
+  final bool enabled;
   final List<TextInputFormatter> inputFormatters;
   final TextEditingController controller;
   final ValueChanged<String> onChanged;
@@ -21,6 +23,8 @@ class Input extends StatelessWidget {
     this.hint,
     this.trailing,
     this.autofocus = false,
+    this.hidden = false,
+    this.enabled = true,
     this.inputFormatters,
     this.controller,
     this.onChanged,
@@ -30,52 +34,58 @@ class Input extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-      height: 55,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(right: 32),
-            child: leading,
-          ),
-          Padding(
-            padding: EdgeInsets.only(right: 5, bottom: 3),
-            child: Text(label, style: CustomStyle.labelStyle),
-          ),
-          Expanded(
-            child: Padding(
-              padding: EdgeInsets.only(bottom: 2),
-              child: TextField(
-                controller: controller,
-                autofocus: autofocus,
-                onChanged: onChanged,
-                onEditingComplete: onEditingComplete,
-                onSubmitted: onChanged,
-                cursorColor: Colors.orangeAccent,
-                cursorWidth: 1.5,
-                keyboardType: TextInputType.number,
-                textAlign: TextAlign.right,
-                inputFormatters: inputFormatters,
-                decoration: InputDecoration(
-                  hintText: hint,
-                  hintStyle: CustomStyle.hintStyle,
-                  contentPadding: EdgeInsets.all(0),
-                  border: InputBorder.none,
-                ),
+    return hidden
+        ? Container()
+        : Opacity(
+            opacity: enabled ? 1 : 0.5,
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+              height: 55,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(right: 32),
+                    child: leading,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(right: 5, bottom: 3),
+                    child: Text(label ?? '', style: CustomStyle.labelStyle),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.only(bottom: 2),
+                      child: TextField(
+                        controller: controller,
+                        autofocus: autofocus,
+                        enabled: enabled,
+                        onChanged: onChanged,
+                        onEditingComplete: onEditingComplete,
+                        onSubmitted: onChanged,
+                        cursorColor: Colors.orangeAccent,
+                        cursorWidth: 1.5,
+                        keyboardType: TextInputType.number,
+                        textAlign: TextAlign.right,
+                        inputFormatters: inputFormatters,
+                        decoration: InputDecoration(
+                          hintText: hint,
+                          hintStyle: CustomStyle.hintStyle,
+                          contentPadding: EdgeInsets.all(0),
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                  ),
+                  trailing != null
+                      ? Container(
+                          width: 44,
+                          padding: EdgeInsets.only(left: 3, bottom: 3),
+                          child: trailing,
+                        )
+                      : Container(),
+                ],
               ),
             ),
-          ),
-          trailing != null
-              ? Container(
-                  width: 44,
-                  padding: EdgeInsets.only(left: 3, bottom: 3),
-                  child: trailing,
-                )
-              : Container(),
-        ],
-      ),
-    );
+          );
   }
 }
